ea34832d0b8bb7f131926eb9c66f5b67404c58cb
jonnow <jonnowitts@gmail.com>
2026-02-04 09:01:38 +0000

    Got basics of JWT auth working

--- routes/user.js
+++ routes/user.js
@@ -1,30 +1,34 @@
-
+const user = { email: 'jonno@email.com', name: 'Jonathan', role: 'Admin' }
 
 const userRoutes = async (fastify, options) => {
   fastify.get('/login', async (request, reply) => {
-    // return login form
+    // This is here for ease of development/testing, will move to the login POST later
     const token = fastify.jwt.sign({
       payload: {
-        email: 'jonno@email.com', firstName: 'Jonathan', role: 'Admin'
+        email: user
       }
     })
+
+    // return login form
     return reply.send({ 'got to Login': token });
   });
 
   fastify.post('/login', async (request, reply) => {
-    // check for user
-    const user = { email: "jonnowitts@gmail.com" }
+    // check for user in the POST
+    // To do - Set this to check the request.body for user
     if (user != null) {
       try {
+        // Create the JWT for the login
         const token = fastify.jwt.sign({
-          payload: {
-            email: 'jonno@email.com', firstName: 'Jonathan', role: 'Admin'
-          }
+          payload: user
         })
 
-        debugger
-        // send email
+        // ? - To do: Save the token in the database?
+
+        // Send email
         await sendMagicLinkEmail({ email: user.email, token })
+
+        // To do: If the email couldn't be send, throw an error.
       } catch (error) {
         return reply.send('Error logging in. Please try again.');
       }
@@ -34,22 +38,49 @@ const userRoutes = async (fastify, options) => {
 
   fastify.get('/verify', async (request, reply) => {
     const token = request.query.token
-    if (token == null) return reply.status(401)
+
+    // If no token, send unauthorised error
+    if (token == null) {
+      return reply.send({ status: 401 })
+    }
+
+    // We have a token...
     try {
-      const decodeToken = token
-      const user = '' //look up user
-      return reply.send(`Authorised as ${user}`)
+      // Authorise the token
+      const authToken = fastify.jwt.verify(token)
+
+      // Set the payload object
+      const payload = authToken.payload.email //look up user
+
+      // Look up user in DB
+      // to do
+
+      // If we don't find the user, send back a generic error
+      // To do: update this if to use the DB checked email
+      if (!payload.email) {
+        return reply.send('An error occurred, please try again')
+      }
+
+      // If we've found the user in the database, authorise them
+      return reply.send(`Authorised as ${payload.name}`)
+
     } catch (error) {
-      return reply.status(401)
+      return reply.send({ error: error.message })
     }
   })
 
   fastify.get('/register', async (request, reply) => {
+    // Send back the register form view
     return reply.send({ 'got to': 'register GET' });
   });
 
   fastify.post('/register', async (request, reply) => {
-    return reply.send({ 'got to': 'register POST' });
+    // Create a JWT with their email
+
+    // Send email to user with login JWT
+
+    // Send back a message to check email
+    return reply.send('Thank you for registering, please check your email for a sign-in link.');
   });
 }
 
